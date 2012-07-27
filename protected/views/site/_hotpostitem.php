ef1cd82a91cd4fdf945bad11c3d3c5e5b6f8fa39
Carlos Belisario <carlos.belisario.gonzalez@gmail.com>
2012-07-26 19:58:44 -0530

    Update protected/views/site/_hotpostitem.php

--- protected/views/site/_hotpostitem.php
+++ protected/views/site/_hotpostitem.php
@@ -3,5 +3,5 @@
    <span>Creado por: <?php echo CHtml::encode($data->username);?> en la fecha <?php echo CHtml::encode(date('d-m-Y h:i:s', $data->dateline));?></span>
    <p>Visto: <?php echo CHtml::encode($data->views); ?></p>  
    <p><?php echo CHtml::encode(substr($data->posts[0]->message, 0, 350)); ?></p>   
-   <a href="http://localhost/mybb/Upload/showthread.php?tid=<?php echo CHtml::encode($data->tid)?>" alt="leer mas">Leer mas</a>
+   <a href="http://urlToMyBB/mybb/Upload/showthread.php?tid=<?php echo CHtml::encode($data->tid)?>" alt="leer mas">Leer mas</a>
 </div>
